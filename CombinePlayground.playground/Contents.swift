804b339185b5400b19dd683c4552c8776058084e
Henry AT <yiperu@gmail.com>
2020-06-05 08:55:20 -0400

    Crear un Subscribe sacando del Publisher (administrar el resultado final del subscriber) y mostrar el consola el resultado de bytes - R

--- CombinePlayground.playground/Contents.swift
+++ CombinePlayground.playground/Contents.swift
@@ -4,3 +4,16 @@ import Combine
 let url = URL(string: "https://applecodingacademy.com/testData/testImages.json")!
 
 let publisher = URLSession.shared.dataTaskPublisher(for: url)
+
+let subscriber = publisher.sink(receiveCompletion: { completion in
+  switch completion {
+  case .finished:
+    print("Termino Correctamente")
+  case .failure(let error):
+    print("Ha ocurrido un Error: \(error)")
+  }
+}, receiveValue: { succes in
+  print(succes.data)
+  print(succes.response)
+})
+
